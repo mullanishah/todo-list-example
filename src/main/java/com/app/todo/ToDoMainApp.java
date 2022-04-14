2022d7d86e299f0243544a6fdae22cf1c7d5c9b9
Shahrukh Mullani <mullanishah@gmail.com>
2022-04-14 23:24:47 +0530

    Initial-project-structure

---
+++ src/main/java/com/app/todo/ToDoMainApp.java
@@ -0,0 +1,13 @@
+package com.app.todo;
+
+/**
+ * Hello world!
+ *
+ */
+public class ToDoMainApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
