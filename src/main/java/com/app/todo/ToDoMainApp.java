1a15496b0aecb7006279d840256655aa614f27f1
Shahrukh Mullani <mullanishah@gmail.com>
2022-04-25 14:53:58 +0530

    feature-logging

--- src/main/java/com/app/todo/ToDoMainApp.java
+++ src/main/java/com/app/todo/ToDoMainApp.java
@@ -1,5 +1,7 @@
 package com.app.todo;
 
+import org.apache.log4j.Logger;
+
 import com.app.todo.ui.TaskCreationUI;
 import com.app.todo.ui.TaskListUI;
 
@@ -9,11 +11,14 @@ import com.app.todo.ui.TaskListUI;
  */
 public class ToDoMainApp {
 	
+	private static final Logger log = Logger.getLogger(ToDoMainApp.class);
+	
     public static void main( String[] args ) {
     	
         try {
+        	log.info("Entered into ToDoMainApp class");
         	TaskListUI uiApp = new TaskListUI();
-        	
+        	log.info("bye");
         }catch (Exception e) {
 			e.printStackTrace();
 		}
